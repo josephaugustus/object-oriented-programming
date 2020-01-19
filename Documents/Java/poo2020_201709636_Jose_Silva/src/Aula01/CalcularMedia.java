7ddc21232b57f1eea1b4c006d3bfdf8c4e791d2a
joseAugustoRodrigues <josephaugustus@outlook.com>
2020-01-19 20:48:41 -0300

    Aula 02

---
+++ Documents/Java/poo2020_201709636_Jose_Silva/src/Aula01/CalcularMedia.java
@@ -0,0 +1,41 @@
+
+package Aula01;
+import java.util.Scanner;
+
+public class CalcularMedia {
+    
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        
+        int nota1, nota2, nota3, nota4;
+        int media, i, contAluno = 0;
+        
+        for(i = 0; i < 4; i++){
+            
+            contAluno++;
+            System.out.println("Aluno " + contAluno + ", digite sua 1ª nota");
+            nota1 = sc.nextInt();
+            
+            System.out.println("Aluno " + contAluno + ", digite sua 2ª nota");
+            nota2 = sc.nextInt();
+            
+            System.out.println("Aluno " + contAluno + ", digite sua 3ª nota");
+            nota3 = sc.nextInt();
+            
+            System.out.println("Aluno " + contAluno + ", digite sua 4ª nota");
+            nota4 = sc.nextInt();
+            
+            // Calcular média
+            media = (nota1 + nota2 + nota3 + nota4) / 4;
+            System.out.println("A média do aluno " + contAluno + " é " + media);
+            
+            // Nota do aluno
+            if( (media >= 0) && (media < 6) ){
+                System.out.println("Reprovado!");
+            } else {
+                System.out.println("Aprovado!");
+            }
+        }       
+    }
+}
